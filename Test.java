d1f75ffc751a8405aa9f4c3de69e34166736b6d5
Mariem Ben Nejma <marioumabennejma2@gmail.com>
2024-02-16 15:05:56 +0100

    Test

--- Test.java
+++ Test.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Test{
     public static void main(String[] args) {
-        
+        Scanner scanner=new Scanner(System.in);
+        System.out.println(" entrer le premier nombre:");
+        double nombre1 =scanner.nextDouble();
+        System.out.println(" entrer le deuxieme nombre:");
+        double nombre2 =scanner.nextDouble();
+        System.out.println(" entrer une operation (+,-,*,/)");
+        scanner.nextLine();
+        String op =scanner.nextLine();
+        double resultat=0;
+        switch (op) {
+            case "+":
+                resultat=Calculatrice.addition(nombre1, nombre2);
+                break;
+            case "-":
+                resultat=Calculatrice.soustraction(nombre1, nombre2);
+                break;
+            case "*":
+                resultat=Calculatrice.multiplaction(nombre1, nombre2);
+                break;
+            case "/":
+                resultat=Calculatrice.division(nombre1, nombre2);
+                break;
+            default:
+                System.out.println("non valide");
+                break;
+        }
+        System.out.println("le résultat est :"+resultat);
+        scanner.close();
+
     }
 }
